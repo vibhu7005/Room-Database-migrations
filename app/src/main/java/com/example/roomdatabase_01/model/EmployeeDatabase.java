0990575a12651658c7e9aca39f7fbd192175ad62
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-01-09 12:35:11 +0530

    Added migrations from Db version 2 to 3.

--- app/src/main/java/com/example/roomdatabase_01/model/EmployeeDatabase.java
+++ app/src/main/java/com/example/roomdatabase_01/model/EmployeeDatabase.java
@@ -5,15 +5,25 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = EmployeeEntity.class, version = 3, exportSchema = false)
 public abstract class EmployeeDatabase extends RoomDatabase {
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE `Employee-Db` ADD COLUMN `position` INTEGER");
+        }
+    };
     public abstract EmployeeDao getEmployeeDao() ;
     private static EmployeeDatabase employeeDatabase;
 
     public static EmployeeDatabase getInstance(Context context) {
         if (employeeDatabase == null) {
-            employeeDatabase = Room.databaseBuilder(context, EmployeeDatabase.class, "Room_DB").fallbackToDestructiveMigrationFrom(1).build();
+            employeeDatabase = Room.databaseBuilder(context, EmployeeDatabase.class, "Room_DB")
+                    .addMigrations(MIGRATION_2_3)
+                    .fallbackToDestructiveMigrationFrom(1).build();
         }
         return employeeDatabase;
     }
